89252a5f185f8c0381c1b890c4bca23c8bcd8806
Sovan Mukherjee <sovanmukherje@gmail.com>
2021-07-02 15:59:33 +0530

    added eureka server

---
+++ crs-eureka-server/src/main/java/com/crs/eurekaserver/CRSEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.crs.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CRSEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CRSEurekaServerApplication.class, args);
+	}
+
+}
---
+++ crs-eureka-server/src/test/java/com/crs/eurekaserver/CRSEurekaServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.crs.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRSEurekaServerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
